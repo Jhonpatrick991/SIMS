f7fb22da0b90caa4c922db4c041ad66639e42c04
Jxsefumi <josefumi89@gmail.com>
2025-05-15 17:08:33 +0800

    HEHE

--- Tables for SIMS.sql
+++ Tables for SIMS.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Students (
     StudentNumber VARCHAR(20) PRIMARY KEY,
     StudentName VARCHAR(100) NOT NULL,
     Course VARCHAR(100) NOT NULL,
-    SubjectCode VARCHAR(20) PRIMARY KEY,
+    SubjectCode VARCHAR(20) NOT NULL,
     Email VARCHAR(100) NOT NULL UNIQUE
 );
 
@@ -13,7 +13,8 @@ CREATE TABLE IF NOT EXISTS Sections (
 );
 
 CREATE TABLE IF NOT EXISTS Subjects (
-    SubjectCode VARCHAR(20) PRIMARY KEY,
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SubjectCode VARCHAR(20) UNIQUE,
     Unit INT NOT NULL,
     SubjectName VARCHAR(255) NOT NULL,
     TotalSections INT NOT NULL DEFAULT 0,
@@ -29,8 +30,15 @@ CREATE TABLE IF NOT EXISTS Grades (
     Midterm DECIMAL(5,2) NOT NULL DEFAULT 1.0,
     SemiFinal DECIMAL(5,2) NOT NULL DEFAULT 1.0,
     Final DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    Total DECIMAL(5,2) AS ((Prelim + Midterm + SemiFinal + Final) / 4) STORED,
+    GWA DECIMAL(5,2) NOT NULL DEFAULT 1.0,
     PRIMARY KEY (StudentNumber, SubjectCode)
 );
 
+CREATE TABLE IF NOT EXISTS StudentSubjects (
+    StudentNumber VARCHAR(20),
+    SubjectCode VARCHAR(20),
+    PRIMARY KEY (StudentNumber, SubjectCode),
+    FOREIGN KEY (StudentNumber) REFERENCES Students(StudentNumber),
+    FOREIGN KEY (SubjectCode) REFERENCES Subjects(SubjectCode)
+);
 
