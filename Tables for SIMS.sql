a3be4cc84752fd018a40ba7df74f819591611e58
Jhon Patrick Mejico <mejico.jhonpatrick.caballero@gmail.com>
2025-05-16 16:23:21 +0800

    Fixed parent and child row conflicts in data insertion, parents first then child.

--- Tables for SIMS.sql
+++ Tables for SIMS.sql
@@ -1,19 +1,13 @@
 CREATE TABLE IF NOT EXISTS Students (
     StudentNumber VARCHAR(20) PRIMARY KEY,
     StudentName VARCHAR(100) NOT NULL,
-    Course VARCHAR(100) NOT NULL,
-    SubjectCode VARCHAR(20) PRIMARY KEY,
+    SectionName VARCHAR(100),
+    SubjectCode VARCHAR(20) NOT NULL,
     Email VARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Sections (
-    SectionId INT PRIMARY KEY AUTO_INCREMENT,
-    SectionName VARCHAR(100) PRIMARY KEY,
-    StudentsEnrolled INT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Subjects (
-    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SubjectId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     SubjectCode VARCHAR(20) UNIQUE,
     Unit INT NOT NULL,
     SubjectName VARCHAR(255) NOT NULL,
@@ -22,16 +16,23 @@ CREATE TABLE IF NOT EXISTS Subjects (
     Time VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Sections (
+    SectionId INT PRIMARY KEY AUTO_INCREMENT,
+    SectionName VARCHAR(100) UNIQUE NOT NULL,
+    StudentsEnrolled INT DEFAULT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Grades (
-    StudentNumber VARCHAR(20),
-    SubjectCode VARCHAR(20),
-    Semester VARCHAR(10),
-    Prelim DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    Midterm DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    SemiFinal DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    Final DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    GWA DECIMAL(5,2) NOT NULL DEFAULT 1.0,
-    PRIMARY KEY (StudentNumber, SubjectCode)
+    StudentNumber VARCHAR(20) NOT NULL,
+    SubjectCode VARCHAR(20) NOT NULL,
+    Semester VARCHAR(10) NOT NULL,
+    Prelim DECIMAL(5,2) DEFAULT NULL,
+    Midterm DECIMAL(5,2) DEFAULT NULL,
+    SemiFinal DECIMAL(5,2) DEFAULT NULL,
+    Final DECIMAL(5,2) DEFAULT NULL,
+    PRIMARY KEY (StudentNumber, SubjectCode),
+    FOREIGN KEY (StudentNumber) REFERENCES Students(StudentNumber),
+    FOREIGN KEY (SubjectCode) REFERENCES Subjects(SubjectCode)
 );
 
 CREATE TABLE IF NOT EXISTS StudentSubjects (
@@ -41,4 +42,3 @@ CREATE TABLE IF NOT EXISTS StudentSubjects (
     FOREIGN KEY (StudentNumber) REFERENCES Students(StudentNumber),
     FOREIGN KEY (SubjectCode) REFERENCES Subjects(SubjectCode)
 );
-
